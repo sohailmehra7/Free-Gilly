d0ad51cb39c953c2e9a11caaace1f7bddced3feb
Sohail Mehra <sohail@seas.upenn.edu>
2013-10-08 16:15:18 -0400

    Initial commit.

---
+++ Assets/Scripts/fishNavigate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishNavigate : MonoBehaviour {
+	public float moveSpeed ; 
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 1.0f ;
+	gameObject.rigidbody.AddForce(new Vector3(0,0,-3));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.rigidbody.AddForce(new Vector3(0,0,-3));
+		if(Input.GetAxisRaw ("Vertical") > 0 )
+		{
+			gameObject.transform.Translate (0,moveSpeed,0);
+			
+		}
+		if(Input.GetAxisRaw ("Vertical") < 0 )
+		{
+			gameObject.transform.Translate (0,-moveSpeed,0);
+			
+		}
+		if(Input.GetAxisRaw ("Horizontal") > 0 )
+		{
+			gameObject.transform.Translate (moveSpeed,0,0);
+			
+		}
+		if(Input.GetAxisRaw ("Horizontal") < 0 )
+		{
+			gameObject.transform.Translate (-moveSpeed,0,0);
+			
+		}
+	//gameObject.transform.position.Set(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 200.0f ); 
+	}
+}
---
+++ Assets/Scripts/level1Global.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level1Global : MonoBehaviour {
+	public AudioClip backGroundMusic; 
+	// Use this for initialization
+	void Start () {
+	AudioSource.PlayClipAtPoint(backGroundMusic,gameObject.transform.position);
+	//AudioSource.	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
